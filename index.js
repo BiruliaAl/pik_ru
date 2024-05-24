946c23065295f04f618934f8db5651abbb652996
Alena Birulia <birulia.ip@gmail.com>
2024-05-24 13:52:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let swiper = new Swiper('.apartments-slider', {
+    // Optional parameters
+    loop: true,
+    autoHeight: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  
+  });
